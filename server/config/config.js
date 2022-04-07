866d44eadb8da81fd5288f2cd01094d8890ebd32
Dmitriy <red_race_riot@mail.ru>
2022-04-07 13:39:22 +0300

    bdCreate1

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,11 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
-const hbs = require('hbs')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const sessionConfig = require('./sessionConfig')
-const login = require('../middlewares/login')
 
 const config = (app) => {
-  // set
-  app.set('view engine', 'hbs')
-  app.set('views', path.join(process.env.PWD, 'views'))
-  hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'))
-
   // use
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
@@ -21,9 +14,6 @@ const config = (app) => {
   // session
   app.use(cookieParser())
   app.use(session(sessionConfig))
-
-  // middlewares
-  app.use(login)
 }
 
 module.exports = config

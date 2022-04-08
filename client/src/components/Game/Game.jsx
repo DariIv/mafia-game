ece0d0b7df10c2246617a2cc351ccfe02a626f25
Artem <breakingbad171@mail.ru>
2022-04-08 15:25:18 +0300

    chatv1

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -1,88 +1,88 @@
 import React from 'react';
-
+import Chat from '../Chat/Chat';
 import './Game.css'
 
 function Game(props) {
   return (
     <>
-    
-    <section id="poker-skin-v1">
-    
-  
-    <div classname="poker-skin-v1-main-container">
-      <div classname="poker-table">
-        <div classname="poker-table-sub-container">
-          <div classname="poker-table-sub-container-grid">
-            
-            <div classname="poker-table-grid-cell">
-              <h4 id="player-1">Albert459</h4>
-              <p>$5.00</p>
-            </div>
-            
-            <div classname="poker-table-grid-cell center">
-             <div classname="center-flex"> 
+      <Chat />
+      <section id="poker-skin-v1">
 
-      
-             </div>  
-            </div>
-                
-            <div classname="poker-table-grid-cell">
-              <h4 id="player-3">RowdyRobert</h4>
-              <p>$10.00</p>
-            </div>
-                
-            <div classname="poker-table-grid-cell">
-              <h4 id="player-4">Marie-Lou</h4>
-              <p>$1.24</p>
-            </div> 
-            
-              <div classname="poker-table-grid-cell my-cards">
-            </div>
-            
-             <div classname="poker-table-grid-cell">
-              <h4 id="player-6">Ellie-Bellie</h4>
-              <p>$6.00</p>
-            </div>    
-          
-              <div classname="poker-table-grid-cell">
 
-            </div> 
-            
-              <div classname="poker-table-grid-cell">
-               
-            </div> 
-            
-            <div classname="poker-table-grid-cell">
-                <div classname="oponents-cards-container">
-          
+        <div className="poker-skin-v1-main-container">
+          <div className="poker-table">
+            <div className="poker-table-sub-container">
+              <div className="poker-table-sub-container-grid">
+
+                <div className="poker-table-grid-cell">
+                  <h4 id="player-1">Albert45</h4>
+                  <p>$5.00</p>
+                </div>
+
+                <div className="poker-table-grid-cell center">
+                  <div className="center-flex">
+
+
+                  </div>
+                </div>
+
+                <div className="poker-table-grid-cell">
+                  <h4 id="player-3">RowdyRobert</h4>
+                  <p>$10.00</p>
+                </div>
+
+                <div className="poker-table-grid-cell">
+                  <h4 id="player-4">Marie-Lou</h4>
+                  <p>$1.24</p>
+                </div>
+
+                <div className="poker-table-grid-cell my-cards">
+                </div>
+
+                <div className="poker-table-grid-cell">
+                  <h4 id="player-6">Ellie-Bellie</h4>
+                  <p>$6.00</p>
+                </div>
+
+                <div className="poker-table-grid-cell">
+
+                </div>
+
+                <div className="poker-table-grid-cell">
+
+                </div>
+
+                <div className="poker-table-grid-cell">
+                  <div className="oponents-cards-container">
+
+                  </div>
+                </div>
+
+                <div className="poker-table-grid-cell">
+                  <div className="oponents-cards-container">
+
+                  </div>
+                </div>
+
+                <div className="poker-table-grid-cell">
+                </div>
+
+                <div className="poker-table-grid-cell">
+                  <h4 id="player-2">Vincent202</h4>
+                  <p>$2.88</p>
+                </div>
+
+                <div className="poker-table-grid-cell">
+                </div>
+
               </div>
-            </div> 
-            
-            <div classname="poker-table-grid-cell">
-              <div classname="oponents-cards-container">
-          
-              </div>
-            </div> 
-            
-            <div classname="poker-table-grid-cell">     
-            </div>  
+            </div>
 
-            <div classname="poker-table-grid-cell">
-              <h4 id="player-2">Vincent202</h4>
-              <p>$2.88</p>
-            </div> 
-            
-            <div classname="poker-table-grid-cell">
-            </div> 
-            
           </div>
-        </div>   
-        
-    </div>
-   </div>
+        </div>
 
-    
-  </section>
+
+      </section>
     </>
   );
 }
--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { usersReducer } from "./usersReducer";
+import { messReducer } from "./chatReducer";
 
 export const rootReducer = combineReducers({
-  usersReducer
-})
+  usersReducer,
+  messReducer,
+});

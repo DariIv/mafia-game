6d387d01dc2a8765f3ed94984ab4822b1436721a
Artem <breakingbad171@mail.ru>
2022-04-14 15:21:17 +0300

    chatAndRegFix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const sessionRouter = require('./routes/sessionRouter.route');
 
 const { Game, Room, UserInRooms } = require('./db/models');
 
-app.use(cors({ origin: 'http://localhost:4000', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: { origin: '*' },
